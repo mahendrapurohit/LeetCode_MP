69e99980c9a2d61e69fdf12740e811600604a2e3
Mahendra purohit <Mahendrapurohitmewada@gmail.com>
2024-02-12 09:14:39 +0530

    12-02-2024

--- Reverse_number.js
+++ Reverse_number.js
@@ -1,15 +1,43 @@
 
 let reverse = function(x) 
 {  
-
+   if(x > 0)
+   {   
     x= x + " ";
     return x.split("").reverse().join(""); 
-    
-    if (x < 0 )
-   {
-     return  '' + x;
+   } else
+    if (x<0)
+   { 
+     x = x + " ";
+      return "-"+ x.slice(1,-1).split("").reverse().join("");
+     
    }
  };
 
-    let z = reverse(987);
-    console.log(z);  
+    let z = reverse(-987);
+
+    console.log(z);
+
+// let number = -123456789;
+
+// let result = 0;
+
+// while(number>0){
+
+//     rightmost = number%10;
+    
+//     result = result*10 + rightmost;
+    
+//     number = Math.floor(number/10);
+// }
+
+// while(number<0){
+
+//   rightmost = number%10;
+  
+//   result = result*10 + rightmost;
+  
+//   number = Math.floor(number/10);
+// };
+
+// console.log("Reversed number is : " "+ result );
